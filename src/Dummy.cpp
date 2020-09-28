806fa93a7f5a838d65dc85056cb2cc2f6d082ee0
Thomas Roehr <thomas.roehr@dfki.de>
2020-09-28 21:12:35 +0200

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace find_gem;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <find_gem/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    find_gem::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
